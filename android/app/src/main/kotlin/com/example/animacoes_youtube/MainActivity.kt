772ab5d6126e6eaed3682cebd9106f4aad482e2c
Paulo <thelukgaming333@gmail.com>
2025-08-20 12:13:44 -0300

    Iniciando branch

---
+++ android/app/src/main/kotlin/com/example/animacoes_youtube/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.animacoes_youtube
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/animacoes_1.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Animacoes1 extends StatefulWidget {
+  const Animacoes1({super.key});
+
+  @override
+  State<Animacoes1> createState() => _Animacoes1State();
+}
+
+class _Animacoes1State extends State<Animacoes1> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
